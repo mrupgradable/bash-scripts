8b5869d90c58055fdb5378404425ff819a39779e
mrupgradable <me@mrupgradable.me>
2024-11-24 10:17:16 +0000

    fixing machine upgrade + adding additional commands

--- machine/machine.update.sh
+++ machine/machine.update.sh
@@ -1,8 +1,18 @@
-#!/bin.bash
+#!/bin/bash
 # machine upgrade script
 
 echo -e "\nUpgrading machine..."
 
 echo -e "\nUsing APT to update packages..."
-sudo apt update
 
+sudo apt update
+sudp apt upgrade
+sudo apt autoclean
+sudo apt autoremove
+
+echo -e "\nUsing SNAP to update snaps..."
+
+sudo snap refresh --list
+sudo snap refresh
+
+echo -e "\nUpgrading machine DONE"
