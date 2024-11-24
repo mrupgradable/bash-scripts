f07aacd8bc349d3af8a0591d7a2602168690948c
mrupgradable <me@mrupgradable.me>
2024-11-24 10:11:05 +0000

    added machine & start of the day scripts

---
+++ machine/machine.update.sh
@@ -0,0 +1,8 @@
+#!/bin.bash
+# machine upgrade script
+
+echo -e "\nUpgrading machine..."
+
+echo -e "\nUsing APT to update packages..."
+sudo apt update
+
---
+++ start/start.the-day.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# start the day scripts - used at the very 1st part of the day for efficientcy
+
+echo -e "\nRunning start of day scripts..."
+
+$HOME/bash-scripts/ssh/ssh.load.keys.sh
+
+echo -e "\nstart of day - DONE"
+
