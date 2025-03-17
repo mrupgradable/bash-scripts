13da8a54efa3b5682bef45a4feaa9bd5281bce9f
mrupgradable <me@mrupgradable.me>
2025-03-17 09:35:10 +0000

    added k9s via docker

---
+++ k8s/k9s.operate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# operate k8s cluster with k9s (docker'd for isolation)
+#
+# CTL + C (exit) 
+#
+# note: needs adequate roles
+#       needs k8s metrics server installed
+#
+# issues: 1. docker.processes (obtain container id)
+#         2. docker.shell <container-id>
+#         3. k9s info (see log path)
+#         4. investigate log
+
+echo "starting k9s"
+
+docker run --rm -it -v $HOME/.kube/config:/root/.kube/config quay.io/derailed/k9s
