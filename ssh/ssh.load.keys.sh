986d0ed435e0acbb1ed5244feb014b122b82ed11
mrupgradable <me@mrupgradable.me>
2025-04-11 11:52:15 +0100

    added senary castlenet key

--- ssh/ssh.load.keys.sh
+++ ssh/ssh.load.keys.sh
@@ -13,5 +13,8 @@ ssh-add ~/.ssh/mrupgradable_server_ssh
 echo -e "\nload senary cloud compute key"
 ssh-add ~/.ssh/mrupgradable_cloud_compute_ssh
 
+echo -e "\nload senary key (for castlenet)"
+ssh-add ~/.ssh/mrupgradable-senary-castlenet
+
 echo -e "\nlisting keys"
 ssh-add -l
